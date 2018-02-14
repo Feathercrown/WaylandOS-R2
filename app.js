c2ef52b808d65e26999fc7d66b8df81e10d28d63
Klari <crystaltheandroid@gmail.com>
2018-02-14 14:28:43 -0500

    zoopity

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 'use strict';
 
-//Libraries and stuff
+// Module and Library requirements
 const DiscordClient = require('discord.js');
+const fs = require('fs');
+
+// Check for token and throw if it doesn't exist
+try {
+    const token = require(`./wayland_token.txt`);
+}
+catch (err) {
+    throw 'oopsie. ' + err;
+}
