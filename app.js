baaf02ef1473e21ce356f63b489c340710b620bf
Cubic-Zirconia <crystaltheandroid@gmail.com>
2018-02-21 11:49:07 -0500

    alright it should be fixed now

--- app.js
+++ app.js
@@ -6,13 +6,8 @@ const fs = require('fs');
 
 // Check for token and throw if it doesn't exist
 try {
-<<<<<<< HEAD
     const token = fs.readFileSync("./wayland_token.txt", "utf-8");
     console.log(token);
-=======
-    var fiel = fs.readFileSync("./wayland_token.txt", "utf-8");
-    console.log(fiel);
->>>>>>> a48ced11fa8000c7efc8e801954b5a0df6f1881a
 }
 catch (err) {
     throw `Ohno! You didn't install the token file! :(\n[${err}]`;
