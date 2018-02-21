0d074fbe8aa4d718984664f68f173c5f7851f913
Cubic-Zirconia <crystaltheandroid@gmail.com>
2018-02-21 11:38:53 -0500

    trying out a new dev environment i can use anywhere and keep online anytime

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 
 // Check for token and throw if it doesn't exist
 try {
-    
+    const token = fs.readFileSync("./wayland_token.txt", "utf-8");
     console.log(token);
 }
 catch (err) {
